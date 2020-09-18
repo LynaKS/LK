e439a116050effc1c7bdd1c3c262a3a6018ec1d9
LynaKS <46923273+LynaKS@users.noreply.github.com>
2020-09-18 10:07:48 +0300

    Add files via upload

---
+++ Sport/main.js
@@ -0,0 +1,19 @@
+let mybutton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "block";
+    }
+};
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
